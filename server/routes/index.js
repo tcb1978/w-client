a0d753cb3eca8795bf000cb7b5eb5544c3c35412
Matthew Eldredge <mattheweldredge@gmail.com>
2018-08-18 22:05:46 -0700

    add pagination logic

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,8 +5,18 @@ const royaltiesData = require('../data/royalties.json');
 
 /* GET home page. */
 router.get('/songs', (req, res, next) => {
+  const { page = "1",  limit = "50"} = req.query;
+
+  const itemsToTake = +limit;
+  const startingIndex = page >= 1 ? (+page - 1) * itemsToTake : 0;
+  const endingIndex = (() => {
+    const pageLength = startingIndex + (+limit);
+    const lastIndex = songsData.items.length - 1;
+    return pageLength > lastIndex ? lastIndex : pageLength;
+  })();
+  
   res.send({
-    items: songsData.items.slice(0, 49).map(s => {
+    items: songsData.items.slice(startingIndex, endingIndex).map(s => {
       s.imagePath = 'https://www.popsci.com/sites/popsci.com/files/styles/655_1x_/public/images/2017/10/terrier-puppy.jpg?itok=Ppdi06hH&fc=50,50';
       return s
     })
