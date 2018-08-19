8475046781519ba8fb1797559a876b8755dfbe4d
Matthew Eldredge <mattheweldredge@gmail.com>
2018-08-18 21:41:32 -0700

    replace image url and reduce payload size

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,12 @@ const royaltiesData = require('../data/royalties.json');
 
 /* GET home page. */
 router.get('/songs', (req, res, next) => {
-  res.send(songsData);
+  res.send({
+    items: songsData.items.slice(0, 49).map(s => {
+      s.imagePath = 'https://www.popsci.com/sites/popsci.com/files/styles/655_1x_/public/images/2017/10/terrier-puppy.jpg?itok=Ppdi06hH&fc=50,50';
+      return s
+    })
+  });
 });
 
 router.get('/songs/:id', (req, res) => {
